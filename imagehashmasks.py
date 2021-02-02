0cb3a5ab1e63c4af7be9aa8df331d6086c0d8d20
banteg <banteeg@gmail.com>
2021-02-02 09:26:59 +0300

    fix: simplify

--- imagehashmasks.py
+++ imagehashmasks.py
@@ -5,18 +5,14 @@ import imagehash
 from PIL import Image
 
 
-def find_by_image(mask, masks):
-    sample_hash = imagehash.phash(Image.open(mask))
-    return min(masks, key=lambda x: imagehash.hex_to_hash(x["phash"]) - sample_hash)
-
-
 @click.command()
 @click.argument("path", type=click.Path(exists=True, dir_okay=False))
 def main(path):
     masks = list(csv.DictReader(open("masks.csv")))
-    mask = find_by_image(path, masks)
+    sample = imagehash.phash(Image.open(path))
+    mask = min(masks, key=lambda x: imagehash.hex_to_hash(x["phash"]) - sample)
     index = (16384 - 10141 + int(mask["index"])) % 16384
-    print(f"https://www.thehashmasks.com/detail/{index}")
+    print(f"{mask['url']}\nhttps://www.thehashmasks.com/detail/{index}")
 
 
 if __name__ == "__main__":
