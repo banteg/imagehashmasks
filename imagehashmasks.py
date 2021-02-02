b67aadce9b5a48a23937e42e14ed09b622306839
banteg <banteeg@gmail.com>
2021-02-02 09:40:03 +0300

    feat: crop resistent hash

--- imagehashmasks.py
+++ imagehashmasks.py
@@ -1,7 +1,7 @@
 import csv
 
 import click
-import imagehash
+from imagehash import ImageMultiHash, crop_resistant_hash, hex_to_hash, phash
 from PIL import Image
 
 
@@ -9,8 +9,9 @@ from PIL import Image
 @click.argument("path", type=click.Path(exists=True, dir_okay=False))
 def main(path):
     masks = list(csv.DictReader(open("masks.csv")))
-    sample = imagehash.phash(Image.open(path))
-    mask = min(masks, key=lambda x: imagehash.hex_to_hash(x["phash"]) - sample)
+    im = Image.open(path)
+    sample = crop_resistant_hash(im, phash)
+    mask = min(masks, key=lambda x: ImageMultiHash([hex_to_hash(x["phash"])]) - sample)
     index = (16384 - 10141 + int(mask["index"])) % 16384
     print(f"{mask['url']}\nhttps://www.thehashmasks.com/detail/{index}")
 
